852082add14ee13fc00f490957a16e0a0d6e4a24
asacha <asc_stb@hotmail.com>
2018-09-05 00:33:13 -0400

    Actualización Semana 1 - Día 2

--- Semana1/Dia1/HelloWorld/app/src/main/java/com/example/user/helloworld/MainActivity.java
+++ Semana1/Dia1/HelloWorld/app/src/main/java/com/example/user/helloworld/MainActivity.java
@@ -3,10 +3,18 @@ package com.example.user.helloworld;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName() + "_TAG";
+    private EditText etName;
+    private TextView tvNameResult;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +22,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d(TAG, "onCreate: ");
+
+        etName = findViewById(R.id.etName);
+        //Button btnShow = findViewById(R.id.btnShow);
+        tvNameResult = findViewById(R.id.tvNameResult);
     }
 
+    public void onShowClick(View view) {
+        Toast.makeText(this, R.string.text_button_clicked, Toast.LENGTH_SHORT).show();
+
+        tvNameResult.setText(getString(R.string.text_hi_button) + etName.getText());
+    }
+
+    //region Activity Life Cycle
     @Override
     protected void onStart() {
         super.onStart();
@@ -51,4 +70,5 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         Log.d(TAG, "onDestroy: ");
     }
+    //endregion
 }
